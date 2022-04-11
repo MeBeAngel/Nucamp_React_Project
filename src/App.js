be523f067abe0d0c27f5a688b7371f6ad5ef2736
Angel Gonzalez <58734248+MeBeAngel@users.noreply.github.com>
2022-04-10 20:54:58 -0400

    add react router params

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
@@ -7,9 +8,11 @@ import './App.css';
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Main />
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <Main />
+                </div>
+            </BrowserRouter>
         );
     }
 }
